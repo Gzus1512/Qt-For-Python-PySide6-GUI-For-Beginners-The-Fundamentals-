2cf897d3c3277d77ccefc77ae0f01d020f9e2951
Jesus Rosas <jrb.2512@gmail.com>
2023-05-10 07:59:15 -0600

    Loading ui file directly using QFile

--- 4.QtDesigner/4.QDesigner_QUiLoader/main.py
+++ 4.QtDesigner/4.QDesigner_QUiLoader/main.py
@@ -1,19 +1,31 @@
 import sys
-from PySide6 import QtWidgets
 from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
 
-loader = QUiLoader() #Set up a loader object
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-app = QtWidgets.QApplication(sys.argv)
-window = loader.load("widget.ui", None) #Load the ui - happens at run time!
+    ui_file_name = "widget.ui"
+    ui_file = QFile(ui_file_name)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
 
-def do_something() :
-    print(window.full_name_line_edit.text(),"is a ", window.occupation_line_edit.text())
+    def do_something() :
+        print(window.full_name_line_edit.text(),"is a ", window.occupation_line_edit.text())
 
-#Changing the properties in the form
-window.setWindowTitle("User data")
+    #Changing the properties in the form
+    window.setWindowTitle("User data")
 
-#Accessing widgets in the form
-window.submit_button.clicked.connect(do_something)
-window.show()
-app.exec()
+    #Accessing widgets in the form
+    window.submit_button.clicked.connect(do_something)
+    window.show()
+
+    sys.exit(app.exec())
